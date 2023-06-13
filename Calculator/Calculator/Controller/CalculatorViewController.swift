e449787f765351ee8e84f83580f90201d6a69dee
조호준 <johojun@johojun-ui-MacBookAir.local>
2023-06-13 12:46:18 +0900

    refactor: scrollToBottom에서 불필요한 조건 제거

--- Calculator/Calculator/Controller/CalculatorViewController.swift
+++ Calculator/Calculator/Controller/CalculatorViewController.swift
@@ -211,9 +211,9 @@ class CalculatorViewController: UIViewController {
 extension UIScrollView {
     func scrollToBottom() {
         self.layoutIfNeeded()
-        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height + contentInset.bottom)
-        if (bottomOffset.y > 0) {
-            setContentOffset(bottomOffset, animated: true)
-        }
+        
+        let bottomOffset = CGPoint(x: 0, y: contentSize.height - bounds.size.height)
+        
+        setContentOffset(bottomOffset, animated: true)
     }
 }
